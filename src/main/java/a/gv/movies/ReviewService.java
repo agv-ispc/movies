0fb2aeaea5d9f7634e4ca7a583af6b9f3e76c02c
Desk1 <agv.ispc@gmail.com>
2023-06-27 17:26:59 -0300

    20230627-1726

--- src/main/java/a/gv/movies/ReviewService.java
+++ src/main/java/a/gv/movies/ReviewService.java
@@ -1,12 +1,16 @@
 package a.gv.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewService {
   @Autowired
   private ReviewRepository reviewRepository;
+
+  private MongoTemplate mongoTemplate;
+  
   public Review createReview(String reviewBody, String imdbId) {
     Review review = new Review(reviewBody);
     reviewRepository.insert(review);
